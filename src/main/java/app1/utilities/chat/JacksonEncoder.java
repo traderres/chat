a67f9dc80c7b653ef777a2e9628cf0398f3221a0
traderres <traderres@yahoo.com>
2017-01-29 15:11:47 -0500

    Little more cleanup.  But, encoder/decoder issues remain.

--- src/main/java/app1/utilities/chat/JacksonEncoder.java
+++ src/main/java/app1/utilities/chat/JacksonEncoder.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 import java.io.IOException;
 
 /**
